1c1bffa603b6e2cf9b5363f9f594cb3636b3ae58
Julien Le Mee <julemee@gmail.com>
2023-01-06 11:45:23 -0500

    strat project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+    import Navbar from './components/Navbar';
+    import Content from './components/Content';
+    import Footer from './components/Footer';
+    import Button from '@mui/material/Button';
+    import './App.scss';
+
+    export default function App() {
+        return (
+            <div className='App'>
+                <Navbar />
+                <Content />
+                <Footer />
+                <Button variant="contained"
+                    onClick={() => {
+                      alert('clicked');
+                    }}
+                  > Hello Julien!</Button>
+            </div>
+        )
+    }
